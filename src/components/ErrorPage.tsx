bed3943a96bb8ab4d6e21a74b9df538080abd2a4
Mikhail Golubev <golubev.mikhail10@gmail.com>
2026-03-09 19:25:06 +0200

    Integrate React Router for page navigation and add error handling component

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,9 @@
+import Text from "./ui/Text.tsx";
+
+const ErrorPage = () => {
+    return (
+        <Text text={'O-o-o-p-s! Something went wrong'}/>
+    );
+};
+
+export default ErrorPage;
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,6 +1,7 @@
 import {useContext} from "react";
 import {SWContext} from "../utils/context.ts";
 import Button from "./ui/Button.tsx";
+import {NavLink} from "react-router";
 
 interface NavItemProps {
     itemTitle: string;
@@ -11,7 +12,9 @@ const NavItem = ({itemTitle}: NavItemProps) => {
     const handleClick = () => changePage(itemTitle);
 
     return (
-        <Button text={itemTitle} onClick={handleClick}/>
+        <NavLink to={`/${itemTitle.toLowerCase()}`}>
+            <Button text={itemTitle} onClick={handleClick}/>
+        </NavLink>
     );
 };
 
